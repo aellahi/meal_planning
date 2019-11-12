3188f0a30908f87add024194f24a8ccfc8096a51
Aisha Ellahi <aishaellahi85@gmail.com>
2019-11-11 16:30:21 -0800

    replace ToDoist api with pytodoist

--- grocery_list/grocery_list.py
+++ grocery_list/grocery_list.py
@@ -2,8 +2,9 @@
 # and adds them as items in ToDoist Grocery list
 
 import sys
-from todoist import TodoistAPI
+from pytodoist import todoist
 import pandas as pd
+import json
 
 # Load recipe info
 # List recipes to create grocery list from:
@@ -18,44 +19,21 @@ all_ingredients.sort_values(by='ingredient', inplace=True)
 summed_ingredients = all_ingredients.groupby(['ingredient', 'unit']).sum()
 summed_ingredients.reset_index(inplace=True)
 
-# Load ToDoist project
+# load ToDoist user and password
+with open('todoist_login.json','r') as json_file:
+    user_credentials = json.load(json_file)
 
-#define token
-token=''
+# login to Todoist
+user = todoist.login(user_credentials['user'], user_credentials['password'])
 
-api = TodoistAPI(token=token)
-api.sync()
+# Load Groceries project
+groceries = user.get_project('Groceries')
 
-# Load projects
-Projects = api.state['projects']
-# grab 'Groceries' project_id
-project_id = None
-for project in Projects:
-    if project['name']=='Groceries':
-        project_id = project['id']
-    if project['name']=='Inbox':
-        inbox = project['id']
-
-
-# try adding item to inbox
-api.items.add('test', **{'project_id':inbox})
-api.items.
-# Raise warning if no Groceries project found
-if project_id:
-    print("Found Groceries project!")
-else:
-    print('Warning! No "Groceries" project found.')
-
-'''
-# iterate through summed_ingredients and add to ToDoist grocery list
+# iterate through summed_ingredients and add to Groceries
 for row in summed_ingredients.itertuples():
     tup=(row.ingredient, row.amt)
     str_list = [x for x in map(str,tup)]
-    to_add = '_'.join(str_list)
-    added = api.items.add(to_add, **{'project_id':project_id})
-    if added['id']:
+    to_add = ' '.join(str_list)
+    added = groceries.add_task(to_add)
+    if added.content:
        print(f"Successfully added {to_add}.")
-    break
-
-api.sync()
-'''
